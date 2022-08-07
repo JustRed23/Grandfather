c15d9d0ad2232700498cdd866fdaf55eeed9b3c0
JustRed23 <manuel.bormans@gmail.com>
2022-08-07 19:44:13 +0200

    Small fix

--- src/main/java/dev/JustRed23/grandfather/App.java
+++ src/main/java/dev/JustRed23/grandfather/App.java
@@ -51,7 +51,7 @@ public class App extends Application {
                 .setChunkingFilter(ChunkingFilter.ALL)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
-                .setActivity(Activity.listening("to 1970's hits"));
+                .setActivity(Activity.listening("1970's hits"));
 
         getServicePool().addService(InactivityService.class);
     }
