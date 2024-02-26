e262a40b64b96ff708e794326b07233bfe7690dc
JustRed23 <manuel.bormans@gmail.com>
2024-02-26 13:00:57 +0100

    Fix inactivity service

--- src/main/java/dev/JustRed23/grandfather/utils/InactivityTimer.java
+++ src/main/java/dev/JustRed23/grandfather/utils/InactivityTimer.java
@@ -8,9 +8,10 @@ import dev.JustRed23.jdautils.music.AudioManager;
 import net.dv8tion.jda.api.entities.Guild;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class InactivityTimer {
@@ -19,16 +20,18 @@ public class InactivityTimer {
 
     private static final Map<Guild, AudioEventAdapter> adapters = new ConcurrentHashMap<>();
     private static final Map<Guild, Long> lastActive = new ConcurrentHashMap<>();
+    private static final List<Guild> pendingRemoval = new CopyOnWriteArrayList<>();
 
     public static AudioEventAdapter create(Guild guild) {
         if (adapters.containsKey(guild)) return adapters.get(guild);
 
         final AudioEventAdapter audioEventAdapter = new AudioEventAdapter() {
             public void onTrackStart(AudioPlayer player, AudioTrack track) {
-                lastActive.remove(guild);
+                pendingRemoval.add(guild);
             }
 
             public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+                pendingRemoval.remove(guild);
                 lastActive.put(guild, System.currentTimeMillis());
             }
         };
@@ -39,19 +42,23 @@ public class InactivityTimer {
     @ApiStatus.Internal
     public static void check() {
         long now = System.currentTimeMillis();
-        for (Map.Entry<Guild, Long> entry : new HashMap<>(lastActive).entrySet()) {
-            final Guild guild = entry.getKey();
-            if (!AudioManager.has(guild) || !AudioManager.get(guild).isConnected()) {
-                lastActive.remove(guild);
-                adapters.remove(guild);
-                continue;
+
+        lastActive.entrySet().removeIf(entry -> {
+            Guild key = entry.getKey();
+
+            if (pendingRemoval.contains(key)) {
+                pendingRemoval.remove(key);
+                return true;
             }
 
             if (now - entry.getValue() > INACTIVITY_THRESHOLD) {
-                AudioManager.get(guild).disconnect();
-                lastActive.remove(guild);
-                adapters.remove(guild);
+                if (AudioManager.has(key) && AudioManager.get(key).isConnected())
+                    AudioManager.get(key).disconnect();
+                adapters.remove(key);
+                pendingRemoval.remove(key);
+                return true;
             }
-        }
+            return false;
+        });
     }
 }
